3f27ec3222271440e6636e3e7bf1059aacbbbf3a
MinWook6457 <minuk6457@gmail.com>
2025-03-13 21:41:44 +0900

    init : svelte kit start

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let name;
+
+  function toggleName() {
+    if (name === "world") {
+      name = "svelte";
+    } else {
+      name = "world";
+    }
+  }
+</script>
+
+<main>
+  <h1>안녕! {name}!</h1>
+  <button on:click="{toggleName}">토글 name</button>
+  <p>
+    <a href="https://svelte.dev/tutorial">Svelte 튜토리얼</a>에 방문해서 Svelte
+    앱을 반드는 방법을 배워보세요.
+  </p>
+</main>
